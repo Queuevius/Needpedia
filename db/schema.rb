efa304faf919ee5b9a68e1f2c20700b24b4b3f80
Shairyar Baig <shairyarbaig@gmail.com>
2020-09-07 00:52:24 +0500

    small fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_06_183958) do
+ActiveRecord::Schema.define(version: 2020_09_04_170733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -137,7 +137,6 @@ ActiveRecord::Schema.define(version: 2020_09_06_183958) do
     t.string "notifiable_type"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "post_id"
   end
 
   create_table "post_tokens", force: :cascade do |t|
