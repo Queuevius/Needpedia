458a6f5a54228106860cf364de73d048b37aa419
Murtaza khan <murtazazarkhwan@gmail.com>
2021-11-29 12:18:30 +0500

    map icon changed, proper not allowed message for private posts, text improved on post form

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,6 +1,6 @@
 module PostsHelper
   def allowed_to_see?(post, current_user)
-    return false unless current_user
+    return true unless current_user
 
     post.private? && !post.private_users.include?(current_user)
     case post.post_type
