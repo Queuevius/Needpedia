309f044c4349b3de7c10831097b3e5247f41b83a
Murtaza Khan <murtazazarkhwan@gmail.com>
2022-01-17 15:22:36 +0500

    changed host url for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,6 +110,6 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.action_mailer.default_url_options = { :host => 'www.needpedia.org' }
+  config.action_mailer.default_url_options = { host: 'https://needpedia.org/' }
   config.action_mailer.raise_delivery_errors = true
 end
