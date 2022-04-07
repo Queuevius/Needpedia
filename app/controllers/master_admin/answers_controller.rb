40f9e28418f7359d2ace1d21eccdcc46fee20a51
Murtaza khan <murtazazarkhwan@gmail.com>
2022-04-07 16:07:15 +0500

    add approve user option in users collection

--- app/controllers/master_admin/answers_controller.rb
+++ app/controllers/master_admin/answers_controller.rb
@@ -3,6 +3,7 @@ module MasterAdmin
     def approve_user
       answer = Answer.find(params[:answer_id])
       user = answer.user
+      redirect_to_page = params[:redirect_to]
       if answer.blank? || user.blank?
         flash[:alert] = 'Can not find this User'
       else
@@ -10,10 +11,10 @@ module MasterAdmin
         user.send_confirmation_instructions.deliver
         flash[:notice] = 'Confirmation link has been sent on user email'
       end
-      redirect_to master_admin_answer_path(answer)
+      redirect_to redirect_to_page == "answer_page" ? master_admin_answer_path(answer) : unconfirmed_users_master_admin_users_path
     rescue StandardError => e
       flash[:alert] = e.message
-      redirect_to master_admin_answer_path(answer)
+      redirect_to master_admin_users_path
     end
   end
 end
