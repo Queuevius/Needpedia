8698b1cbd44f8c5acc6452c20a7085f1920e94ec
Murtaza khan <murtazazarkhwan@gmail.com>
2022-04-16 17:04:30 +0500

    improved rating modal, tooltip for answer on hover on admin dashboard

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -3,15 +3,17 @@ class RatingsController < ApplicationController
 
   def change_rating
     @rating = Rating.new(rating_params)
+    @post = Post.includes(:ratings).find(@rating.rateable_id)
 
     old = Rating.where(user_id: current_user.id, rateable_id: @rating.rateable_id, rateable_type: @rating.rateable_type)
-    if old.present?
+    if old.pluck(:rating).include?(@rating.rating)
       old.destroy_all
+      @rating = nil
+    else
+      @rating.user = current_user
+      @rating.save
+      old.where.not(id: @rating.id).destroy_all
     end
-
-    @rating.user = current_user
-    @post = Post.includes(:ratings).find(@rating.rateable_id)
-    @rating.save
   end
 
   private
