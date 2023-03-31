43553fea04cd43953974c3d1c31e7eb680ff5632
John Bonjour <whitewolfzhang@gmail.com>
2023-03-31 04:29:26 -0700

    Updated the seed generation. The older version fails the seeding process due to mismatching the required password pattern. (This seed generation, however, still cannot be used, even though it can passthrough the password check. When using a user generated by seed to login, website rejects the request due to "You have to confirm your email address before continuing." There will be a future commit(/fix).)

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,7 @@
       first_name: Faker::Name.first_name,
       last_name: Faker::Name.last_name,
       email: Faker::Internet.unique.email,
-      password: 'password',
+      password: 'Password123#',
       # image_file_name: rand(1..16)
   )
   u.save!
