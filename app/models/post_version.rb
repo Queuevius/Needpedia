3b87942d7c9b7d1fe6e0cfe5b286293862617e50
Murtaza Khan <murtazazarkhwan@gmail.com>
2023-07-04 12:30:29 +0500

    fixed issue where deleted versions continued to display on the site

--- app/models/post_version.rb
+++ app/models/post_version.rb
@@ -4,4 +4,17 @@ class PostVersion < ApplicationRecord
   has_many :flags, as: :flagable, dependent: :destroy
   has_many :deletion_requests, dependent: :destroy
   has_rich_text :content
+  after_destroy :update_post_content
+
+  private
+
+  def update_post_content
+    last_version = post.post_versions.last
+    if last_version.present?
+      post.update(content: last_version.content)
+      last_version.update(active: true)
+    else
+      DeletePostService.new(post.id, post.user.id).delete_post
+    end
+  end
 end
