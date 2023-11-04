3bf2940685a0fc405e894f2d88943898a26475d6
Mubasher Ayub <mubasherrajput3@gmail.com>
2023-11-04 12:18:47 +0500

    Committing test/models/how_to_test.rb

--- test/models/how_to_test.rb
+++ test/models/how_to_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class HowToTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should have one attached question_picture" do
+    how_to = HowTo.new
+    how_to.question_picture.attach(io: File.open('test/fixtures/sample_image.jpg'), filename: 'sample_image.jpg', content_type: 'image/jpeg')
+
+    assert how_to.valid?
+  end
+
+  test "should have one attached answer_picture" do
+    how_to = HowTo.new
+    how_to.answer_picture.attach(io: File.open('test/fixtures/sample_image.jpg'), filename: 'sample_image.jpg', content_type: 'image/jpeg')
+
+    assert how_to.valid?
+  end
 end
