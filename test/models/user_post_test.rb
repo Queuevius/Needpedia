c8f73519983ab17ef49b376ce66ef3f1230166a9
Mubasher Ayub <mubasherrajput3@gmail.com>
2023-11-04 12:18:47 +0500

    Committing test/models/user_post_test.rb

--- test/models/user_post_test.rb
+++ test/models/user_post_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class UserPostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.create(username: 'user1', email: 'user1@example.com', password: 'password')
+    @post = Post.create(title: 'Sample Post', user: @user)
+  end
+
+  test 'should be valid with valid attributes' do
+    user_post = UserPost.new(user: @user, post: @post)
+    assert user_post.valid?
+  end
+
+  test 'should belong to a user' do
+    association = UserPost.reflect_on_association(:user)
+    assert_equal :belongs_to, association.macro
+  end
+
+  test 'should belong to a post' do
+    association = UserPost.reflect_on_association(:post)
+    assert_equal :belongs_to, association.macro
+  end
 end
