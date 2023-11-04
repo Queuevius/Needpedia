ab8e1fff6381cc0499e012a9ed23fb818ca28cc1
Mubasher Ayub <mubasherrajput3@gmail.com>
2023-11-04 12:18:47 +0500

    Committing test/models/answer_test.rb

--- test/models/answer_test.rb
+++ test/models/answer_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class AnswerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should be valid" do
+    user = User.create(name: "John")
+    question = Question.create(title: "Example Question")
+    answer = Answer.new(user: user, question: question, content: "This is an answer.")
+    assert answer.valid?
+  end
+
+  test "should belong to a user" do
+    answer = Answer.new(question: Question.new, content: "This is an answer.")
+    assert_not answer.valid?
+    assert_includes answer.errors[:user], "must exist"
+  end
+
+  test "should belong to a question" do
+    answer = Answer.new(user: User.new, content: "This is an answer.")
+    assert_not answer.valid?
+    assert_includes answer.errors[:question], "must exist"
+  end
 end
