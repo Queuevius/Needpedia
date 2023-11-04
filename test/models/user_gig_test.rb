0fde130ccc9f9846e684626c9b28780c83229186
Mubasher Ayub <mubasherrajput3@gmail.com>
2023-11-04 12:18:47 +0500

    Committing test/models/user_gig_test.rb

--- test/models/user_gig_test.rb
+++ test/models/user_gig_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class UserGigTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.create(username: 'user1', email: 'user1@example.com', password: 'password')
+    @gig = Gig.create(title: 'Sample Gig', user: @user)
+  end
+
+  test 'should be valid with valid attributes' do
+    user_gig = UserGig.new(user: @user, gig: @gig)
+    assert user_gig.valid?
+  end
+
+  test 'should belong to a user' do
+    association = UserGig.reflect_on_association(:user)
+    assert_equal :belongs_to, association.macro
+  end
+
+  test 'should belong to a gig' do
+    association = UserGig.reflect_on_association(:gig)
+    assert_equal :belongs_to, association.macro
+  end
 end
