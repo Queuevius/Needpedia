aaa6d4abcc3e38cf13d0f593df7cf5e5a909498e
Murtaza khan <63797576+murtazazarkhwan@users.noreply.github.com>
2024-05-15 12:42:26 +0500

    allow admins to upload posts from csv files (#124)

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,6 @@
 ActiveRecord::Schema.define(version: 2024_02_28_062109) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "hstore"
   enable_extension "plpgsql"
   enable_extension "uuid-ossp"
 
