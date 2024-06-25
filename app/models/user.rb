f4443fbef59554ff815ebfda2255a2225d69749f
Murtaza khan <63797576+murtazazarkhwan@users.noreply.github.com>
2024-06-25 12:04:46 +0500

    Fix password complexity requirements (#126)

--- app/models/user.rb
+++ app/models/user.rb
@@ -139,7 +139,7 @@ class User < ApplicationRecord
   end
 
   def password_complexity
-    return if password.blank? || password =~ /(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])/
+    return if password.blank? || password =~ /(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*_-])/
 
     errors.add :password, 'Complexity requirement not met. Please use: 1 uppercase, 1 lowercase, 1 digit and 1 special character'
   end
