3b64c6588ebd86cdc8383010d1843cd836ad7056
Murtaza Khan <murtazazarkhwan@gmail.com>
2024-07-15 15:51:24 +0500

    fixed user invitation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   include DeviseTokenAuth::Concerns::User
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :masqueradable, :registerable, :recoverable, :rememberable, :validatable, :omniauthable, :confirmable
+  devise :masqueradable, :registerable, :recoverable, :rememberable, :validatable, :omniauthable, :confirmable, :invitable
 
 
   after_create :add_default_credit, :create_admin_notifications, :make_friend_with_mascot, :create_user_tutorials
