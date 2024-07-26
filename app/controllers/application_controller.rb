3cee0c4b89459b5c0288cf71ac8b5401967cddff
Murtaza khan <63797576+murtazazarkhwan@users.noreply.github.com>
2024-07-26 10:15:26 +0500

    implement two factor authentication (#130)
    * implement two factor authentication
    
    * fixed user invitation
    
    * refactor code
    
    * Add backup codes, redirect to OTP path if not enabled, and remove disable OTP

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,13 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :masquerade_user!
   before_action :set_ransack, :conversations
+  before_action :check_otp, unless: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :message_notifications, :track_notifications, :daily_notifications, :all_notifications, :default_group_id])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :message_notifications, :track_notifications, :daily_notifications, :all_notifications, :default_group_id, :otp_attempt])
     devise_parameter_sanitizer.permit(:accept_invitation, keys: [:name])
   end
 
@@ -24,5 +25,14 @@ class ApplicationController < ActionController::Base
 
   def check_nuclear_note
     redirect_to nuclear_note_path if Setting.nuclear_note_active?
+  end
+
+  def check_otp
+    if !current_user&.otp_required_for_login
+      return if controller_name == "profile" && action_name == "otp" || controller_name == "users"
+
+      flash[:alert] = "Please complete the two-factor authentication setup to continue."
+      redirect_to otp_path
+    end
   end
 end
