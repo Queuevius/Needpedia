4e315111490c6f1dc1c6951623b4e4aa5a9857a4
Murtaza khan <63797576+murtazazarkhwan@users.noreply.github.com>
2024-08-19 10:50:48 +0500

    July 2024 release (#136)
    * Update ai chatbot link (#133)
    
    * Updated the AI chatbot link
    
    * add token to AI chatbot link
    
    * implement two factor authentication (#130)
    
    * implement two factor authentication
    
    * fixed user invitation
    
    * refactor code
    
    * Add backup codes, redirect to OTP path if not enabled, and remove disable OTP
    
    * Implement password change notifications & fix Devise mailer (#134)
    
    * Fix PDF links authentication with Bearer token (#135)
    
    * introduce account locking (#132)
    
    * Fixes/fcm notification (#131)
    
    * fixed fcm notification service for mobile app
    
    * fixed fcm notification service for mobile app
    
    * removed extra lines
    
    ---------
    
    Co-authored-by: Mohsin <mohd.mohsinijaz@gmail.com>
    Co-authored-by: Murtaza Khan <murtazazarkhwan@gmail.com>
    
    * Allow access to user_assistant_documents controller without OTP setup
    
    * Add current_user check in check_otp method and refactor code
    
    * Fix PDF URL
    
    * Added a check to return  if  includes api/
    
    ---------
    
    Co-authored-by: Mubasher Ayub <mubasherrajput3@gmail.com>
    Co-authored-by: Mohsin <mohd.mohsinijaz@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,13 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :masquerade_user!
   before_action :set_ransack, :conversations
+  before_action :check_otp, unless: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :message_notifications, :track_notifications, :daily_notifications, :all_notifications, :default_group_id])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :message_notifications, :track_notifications, :daily_notifications, :all_notifications, :default_group_id, :otp_attempt])
     devise_parameter_sanitizer.permit(:accept_invitation, keys: [:name])
   end
 
@@ -25,4 +26,16 @@ class ApplicationController < ActionController::Base
   def check_nuclear_note
     redirect_to nuclear_note_path if Setting.nuclear_note_active?
   end
+
+  def check_otp
+    return if controller_path.include?("api/")
+
+    return unless current_user && !current_user.otp_required_for_login
+
+    return if controller_name.in?(%w[profile users user_assistant_documents]) && action_name.in?(%w[otp enable_otp_verify])
+
+    flash[:alert] = "Please complete the two-factor authentication setup to continue."
+    redirect_to otp_path
+  end
+
 end
