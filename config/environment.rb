ae3fb80897e1fda2562e312ee44f41220eabe984
Murtaza khan <murtazazarkhwan@gmail.com>
2025-09-26 13:05:04 +0500

    switch to mailjet and update capistrano ruby version

--- config/environment.rb
+++ config/environment.rb
@@ -4,10 +4,11 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
-  domain:         ENV['DOMAIN'],
-  address:        'smtp.sendgrid.net',
-  port:            587,
-  authentication: :plain,
-  user_name:      ENV['USER_NAME'],
-  password:       ENV['PASSWORD']
+  address:              'in-v3.mailjet.com',
+  port:                 587,
+  domain:               (ENV['MAILJET_DOMAIN'] || ENV['DOMAIN']),
+  authentication:       :plain,
+  user_name:            ENV['MAILJET_API_KEY'],
+  password:             ENV['MAILJET_SECRET_KEY'],
+  enable_starttls_auto: true
 }
